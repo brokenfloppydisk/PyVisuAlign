235a44ff606416fc4ad20b25a102d9f3ba0de9e8
Kyle Huang <kylehuang06@gmail.com>
2026-05-06 21:33:46 -0700

    Clean up, remove nifti

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import pyvisualign.app as app
+#!/usr/bin/env python3
+
+
 
 if __name__ == "__main__":
+    import pyvisualign.app as app
+    import os
+
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     app.run()
